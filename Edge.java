11560865ca2de8622f21097a885808c2df3a75f8
Joshua Hahn <joshua.hahnjy@gmail.com>
2022-01-01 22:12:41 +0900

    Edge fields and construcotrs complete

--- Edge.java
+++ Edge.java
@@ -1,3 +1,20 @@
 public class Edge {
 
+    public Vertex source;
+    public Vertex target;
+    public Double distance;
+
+    // Default constructor
+    public Edge(Vertex source, Vertex target) {
+        this.source = source;
+        this.target = target;
+        this.distance = 1.0;
+    }
+
+    // Constructor with distance
+    public Edge(Vertex source, Vertex target, Double distance) {
+        this.source = source;
+        this.target = target;
+        this.distance = distance;
+    }
 }
