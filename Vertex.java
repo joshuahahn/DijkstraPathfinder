296963bb6df749cf2bede9f6b226cd79cb3c7ea9
Joshua Hahn <joshua.hahnjy@gmail.com>
2022-01-01 22:10:18 +0900

    Fields and constructor outlined

--- Vertex.java
+++ Vertex.java
@@ -1,3 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Vertex {
+
+    public String name;
+    private List<Edge> adjacent;
+    public boolean visited;
+    public double cost;
+    public Vertex backpointer;
+
+    // Default constructor
+    public Vertex(String name) {
+        this.name = name;
+        this.adjacent = new LinkedList<Edge>();
+    }
 
 }
