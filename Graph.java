afed4313a656f5f536031c6d01fa73bb725671de
Joshua Hahn <joshua.hahnjy@gmail.com>
2022-01-01 22:21:51 +0900

    Graph constructor

--- Graph.java
+++ Graph.java
@@ -1,3 +1,12 @@
+import java.util.HashSet;
+
 public class Graph {
+
+    HashSet<Vertex> vertices;
+
+    // Default constructor
+    public Graph() {
+        vertices = new HashSet<Vertex>();
+    }
 
 }
