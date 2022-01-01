c4ddf110dd69fdab08ce36bcee9f25fa2d535a0e
Joshua Hahn <joshua.hahnjy@gmail.com>
2022-01-01 22:04:55 +0900

    File IO complete

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+         * / Read in data and create appropriate vertices and edges.
+         * / File format (expected name: "input.txt"):
+         * / Number of available paths
+         * / Start1 End1 Distance(cost)
+         * / Start2 End2 Distance(cost)
+         * / ...
+         */
+        try {
+            File inputFile = new File("input.txt");
+            Scanner sc = new Scanner(inputFile);
+            int numEdges = Integer.parseInt(sc.nextLine());
+            for (int i = 0; i < numEdges; i++) {
+                // If either cities have not been seen before, add them to list of vertices
+                // Create new edge for each line
+            }
+            sc.close();
+        }
+
+        // Error handling
+        catch (FileNotFoundException e) {
+            System.out.println("Input file should be named 'input.txt'. File not found.");
+        }
+
+    }
+}
