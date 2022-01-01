0dc405808846b564a22d73a4437d9476a99d1084
Joshua Hahn <joshua.hahnjy@gmail.com>
2022-01-01 22:38:21 +0900

    Graph initialization complete

--- Main.java
+++ Main.java
@@ -18,19 +18,49 @@ public class Main {
          * / ...
          */
         try {
+            // Text input handling
             File inputFile = new File("input.txt");
             Scanner sc = new Scanner(inputFile);
+
             int numEdges = Integer.parseInt(sc.nextLine());
+
+            // Process every new edge
             for (int i = 0; i < numEdges; i++) {
-                // If either cities have not been seen before, add them to list of vertices
-                // Create new edge for each line
+
+                // Clean data
+                String nextLine = sc.nextLine();
+                String[] contents = nextLine.split(" ");
+                String source = contents[0];
+                String target = contents[1];
+                Double cost = Double.parseDouble(contents[2]);
+
+                // Create new vertices
+                Vertex sourceV = new Vertex(source);
+                Vertex targetV = new Vertex(target);
+
+                if (!graph.getVertices().contains(sourceV))
+                    graph.addVertex(sourceV);
+                if (!graph.getVertices().contains(targetV))
+                    graph.addVertex(targetV);
+
+                // Assume all paths are bilateral, add both edges.
+                Edge sourceToTarget = new Edge(sourceV, targetV, cost);
+                Edge targetToSource = new Edge(targetV, sourceV, cost);
+
+                sourceV.addEdge(sourceToTarget);
+                targetV.addEdge(targetToSource);
+
             }
             sc.close();
         }
 
-        // Error handling
+        // Error handling: file does not exist
         catch (FileNotFoundException e) {
             System.out.println("Input file should be named 'input.txt'. File not found.");
+        }
+
+        catch (NumberFormatException e) {
+            System.out.println("Input file has incorrectly formatted costs.");
         }
 
     }
