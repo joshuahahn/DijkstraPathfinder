73f21af5835f50077b482174b269230de7da1f24
Joshua Hahn <joshua.hahnjy@gmail.com>
2022-01-02 11:11:50 +0900

    CostVertex constructor

--- CostVertex.java
+++ CostVertex.java
@@ -1,3 +1,12 @@
 public class CostVertex {
 
+    // CostVertex is a wrapper class for Vertex and cost.
+    public Vertex vertex;
+    public Double cost;
+
+    // Default constructor
+    public CostVertex(Vertex vertex, Double cost) {
+        this.vertex = vertex;
+        this.cost = cost;
+    }
 }
